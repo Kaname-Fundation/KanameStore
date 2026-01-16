2c1dc90be0cb48db5397f2ac594c8a6d2d90de2b
DemuraAIdev <cyberaioff@gmail.com>
2026-01-16 23:32:57 +0700

    Wolfstein 3D

---
+++ apps/Wolfenstein3D/rspack.config.js
@@ -0,0 +1,51 @@
+const path = require("path");
+const rspack = require("@rspack/core");
+const mode = process.env.NODE_ENV || "development";
+const minimize = mode === "production";
+
+module.exports = {
+    mode,
+    devtool: "source-map",
+    entry: {
+        main: [
+            path.resolve(__dirname, "index.js"),
+        ],
+    },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].js",
+        library: "Wolfenstein3D",
+        libraryTarget: "umd",
+    },
+    externals: {
+        osjs: "OSjs",
+    },
+    optimization: {
+        minimize,
+    },
+    plugins: [
+        new rspack.CopyRspackPlugin({
+            patterns: [
+                { from: "icon.png" },
+                { from: "html5-wolfenstein3D", to: "html5-wolfenstein3D" }
+            ]
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: "builtin:swc-loader",
+                    options: {
+                        jsc: {
+                            parser: {
+                                syntax: "ecmascript",
+                            },
+                        },
+                    },
+                },
+            },
+        ],
+    },
+};
